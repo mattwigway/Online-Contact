8c002ab122cd96d9f2606ac67ecb2e08655a84e2
Matt Conway <mattwigway@gmail.com>
2011-12-27 23:06:23 -0800

    Initial checkin.

---
+++ contact-client.js
@@ -0,0 +1,87 @@
+$(document).ready(function () {
+    var socket = io.connect('http://localhost:7501');
+    
+    // when a connection is established, this flag is enabled
+    var active = false;
+
+    var targetWord;
+    
+    // this is how many letters they have so far
+    var letters;
+
+    var clues = {};
+
+    // server push
+    socket.on('connect', function () {
+	socket.emit('add user', prompt('enter a username'));
+    });
+
+    socket.on('user added', function () {
+	socket.emit('connect to game', prompt('enter a game to connect to. undefined games will be created'));
+    });
+
+    socket.on('connected to game', function () {
+	active = true;
+    });
+
+    socket.on('receive chat', function (user, chat) {
+	$('#chat').prepend($('<p>').text(user + ': ' + chat));
+    });
+
+    socket.on('receive clue', function (clue, word, user) {
+	
+	var htclue = 	    
+	    $('<li>').text(user + ' gives clue ' + clue)
+	    .append($('<form><input type="text" class="answer"/><input type="submit"/></form>')
+		    .submit(function (e) {
+			e.preventDefault();
+			
+			console.log($(this).find('.answer').val(), word);
+
+			if ($(this).find('.answer').val() == word) {
+			    socket.emit('contact', clue, word);
+			}
+		    })
+		   );
+
+	clues[clue + ':' + word] = htclue;
+	
+	$('#clues ul').append(htclue);
+    });	
+
+    socket.on('set word', function (theWord) {
+	targetWord = theWord;
+	$('#word').text(targetWord[0]);
+	letters = 1;
+    });
+
+    socket.on('receive contact', function (clue, word) {
+	// get rid of the clue
+	try {
+	    clues[clue + ':' + word].remove();
+	} 
+	// if the clue was never defined because this is the originating client
+	catch (err) {}
+
+	// give another letter
+	letters += 1;
+	$('#word').text(targetWord.slice(0, letters));
+    });
+
+    $('#clues form').submit(function (e) {
+	e.preventDefault();
+	socket.emit('send clue', $('#clue').val(), $('#clueword').val());
+    });
+
+    $('#wordmaster').submit(function (e) {
+	e.preventDefault();
+	socket.emit('set word and wordmaster', $('#wordmaster-word').val());
+    });
+
+    $('#chatin').submit(function (e) {
+	e.preventDefault();
+	socket.emit('send chat', $('#chatin input').val());
+    });
+});
+			  
+	
---
+++ contact.js
@@ -0,0 +1,69 @@
+/**
+   contact.js: play the game of Contact over long distances
+   @author mattwigway
+ */
+
+var PORT = 7501;
+var IP = 'localhost';
+
+var http = require('http');
+
+var app = http.createServer(handler);
+
+app.listen(PORT, IP);
+var io = require('socket.io').listen(app);
+
+function handler (req, res) {
+    res.writeHead(200);
+    res.end('You are connected to the stream server. Please connect using' +
+	    ' a Contact client.');
+}
+
+// here's the workhorse
+io.sockets.on('connection', function (socket) {
+    console.log('new connection');
+
+    // since it's a closure, user persists into inner functions
+    socket.on('add user', function (user) {
+	socket.emit('user added');
+
+	// now, make these available
+	socket.on('connect to game', function (game) {
+	    socket.join(game);
+	    socket.emit('connected to game');
+	    console.log('broadcasting');
+	    
+	    console.log(user);
+	    // it's a chat message that will look like INFO: mattwigway has joined the game
+	    io.sockets.in(socket.room).emit('receive chat', 'INFO', user + ' has joined the game');
+	    
+	    // TODO: how to send user list?
+	    
+	    // these are only available in a game
+	    socket.on('send clue', function (clue, word) {
+		// people can cheat by looking at the HTTP responses, but this avoids a round-trip to the server
+		// we send the user b/c it may be useful to other players to know who wrote a clue, for inside jokes &c.
+		socket.broadcast.to(game).emit('receive clue', clue, word, user);
+	    });
+
+	    // woohoo!
+	    socket.on('contact', function (clue, word) {
+		io.sockets.in(socket.room).emit('receive contact', clue, word);
+		io.sockets.in(socket.room).emit('receive chat', '', user + ' won clue ' + clue + ' with ' + word);
+	    });
+
+	    socket.on('send chat', function (chat) {
+		io.sockets.in(socket.room).emit('receive chat', user, chat);
+	    });
+
+	    // TODO: some sort of security so that not just anyone can grab wordmaster?
+	    socket.on('set word and wordmaster', function (word) {
+		// each browser caches the whole thing
+		io.sockets.in(socket.room).emit('set word', word);
+		io.sockets.in(socket.room).emit('receive chat', user, 'I am now wordmaster. The letter is ' + word[0]);
+		
+		// TODO: take wordmaster from everyone else
+	    });
+	});
+    });
+});
